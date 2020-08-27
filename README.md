5d9c471e1e6f09d3f97e06111b38f426333e4f81
cjsellwood <c.sellwood22@gmail.com>
2020-08-27 11:51:50 +1000

    Project set up with webpack

---
+++ README.md
@@ -0,0 +1,1 @@
+An app for displaying the weather in a desired location
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      favicon: "./src/sun.svg",
+      title: "Weather",
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    host: "10.0.0.6",
+    port: "5000",
+  }
+};
